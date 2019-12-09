0802d7d76c9572d174bd5c4fa2c79d8af7776384
David de Graaf <d.degraaf@apt-one.com>
2019-12-09 22:13:45 +0100

    UPDATED microbit-player.js

--- microbit_player.js
+++ microbit_player.js
@@ -6,100 +6,132 @@ let positions: string[] = []
 let targetedPositions: string[] = []
 let round = ""
 let isStarted = false
+
 basic.showString("P1")
 basic.pause(200)
+
 while (!(serialConnected)) {
     serial.writeLine("request connection")
+
     basic.showIcon(IconNames.Target)
     basic.pause(100)
 }
+
 basic.pause(200)
 basic.showIcon(IconNames.Yes)
 basic.pause(200)
 basic.showString("Setup")
 basic.pause(200)
 round = "SETUP"
+
 led.plot(0, 0)
 
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    let s = serial.readLine().split(" ")
+    let data = serial.readLine().split(" ")
+
     if (serial.readLine() === "connection accepted") {
         serialConnected = true
+
     } else if (serial.readLine() === "turn") {
         round = "ATTACK"
         right = 0
         left = 0
         basic.clearScreen()
+
         for (let i = 0; i < targetedPositions.length; i++) {
             let pos = targetedPositions.get(i)
-            let pos1 = pos.split(".");
-            led.plot(parseInt(pos1[0]), parseInt(pos1[1]))
+            let posArray = pos.split(".");
+
+            led.plot(parseInt(posArray[0]), parseInt(posArray[1]))
         }
-    } else if (s[0] === "watch") {
+
+    } else if (data[0] === "watch") {
         round = "WATCH"
-        let locationString = s[1]
+
+        let locationString = data[1]
         let locationArray = locationString.split("-")
+
         locationArray.splice(0, 1)
+
         for (let i = 0; i < locationArray.length; i++) {
             let pos = locationArray.get(i)
-            let pos1 = pos.split(".");
-            led.plot(parseInt(pos1[0]), parseInt(pos1[1]))
+            let posArray = pos.split(".");
+
+            led.plot(parseInt(posArray[0]), parseInt(posArray[1]))
         }
-    } else if (s[0] === "remove-ship") {
-        let pos = s[1] + "." + s[2]
+
+    } else if (data[0] === "remove-ship") {
+        let pos = data[1] + "." + data[2]
         let index = positions.indexOf(pos)
+
         if (index >= 0) {
             positions.splice(index, 1)
         }
-    } else if(s[0] === "win") {
-        basic.showString(s[1] + " wins")
+
+    } else if (data[0] === "win") {
+        basic.showString(data[1] + " wins")
     }
 })
 input.onButtonPressed(Button.AB, function () {
     if (round === "SETUP") {
         if (positions.indexOf(right.toString() + "." + left.toString()) >= 0) {
             serial.writeLine(`add_location ${right} ${left}`);
-            setupCount++
+
             positions.push(right.toString() + "." + left.toString())
+            setupCount++
         }
+
         if (setupCount === 6) {
             serial.writeLine("ready")
+
             while (!isStarted) {
                 basic.showIcon(IconNames.Yes)
             }
         }
+    
     } else if (round === "ATTACK") {
         if (!(targetedPositions.indexOf(right.toString() + "." + left.toString()) >= 0)) {
-            serial.writeLine("target " + right + " " + left)
+            targetedPositions.join("" + right.toString() + "." + left.toString())
+
+            serial.writeLine("target " + right.toString() + " " + left.toString())
         }
     }
 })
 input.onButtonPressed(Button.A, function () {
     if (round === "SETUP") {
         basic.clearScreen()
+
         for (let i = 0; i < positions.length; i++) {
-            let s = positions.get(i)
-            let s1 = s.split(".");
-            led.plot(parseInt(s1[0]), parseInt(s1[1]))
+            let pos = positions.get(i)
+            let posArray = pos.split(".");
+
+            led.plot(parseInt(posArray[0]), parseInt(posArray[1]))
         }
+
         if (left == 4) {
             left = 0
         } else {
             left += 1
         }
+
         led.plot(right, left)
+
     } else if (round === "ATTACK") {
         basic.clearScreen()
+
         for (let i = 0; i < targetedPositions.length; i++) {
-            let s = targetedPositions.get(i)
-            let s1 = s.split(".");
-            led.plot(parseInt(s1[0]), parseInt(s1[1]))
+            let pos = targetedPositions.get(i)
+            let posArray = pos.split(".");
+
+            led.plot(parseInt(posArray[0]), parseInt(posArray[1]))
         }
+
         if (left == 4) {
             left = 0
         } else {
             left += 1
         }
+
         led.plot(right, left)
     }
 })
@@ -107,29 +139,38 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     if (round === "SETUP") {
         basic.clearScreen()
+
         for (let i = 0; i < positions.length; i++) {
-            let s = positions.get(i)
-            let s1 = s.split(".")
-            led.plot(parseInt(s1[0]), parseInt(s1[1]))
+            let pos = positions.get(i)
+            let posArray = pos.split(".")
+
+            led.plot(parseInt(posArray[0]), parseInt(posArray[1]))
         }
+
         if (right == 4) {
             right = 0
         } else {
             right += 1
         }
+
         led.plot(right, left)
+
     } else if (round === "ATTACK") {
         basic.clearScreen()
+
         for (let i = 0; i < targetedPositions.length; i++) {
-            let s = targetedPositions.get(i)
-            let s1 = s.split(".")
-            led.plot(parseInt(s1[0]), parseInt(s1[1]))
+            let pos = targetedPositions.get(i)
+            let posArray = pos.split(".")
+
+            led.plot(parseInt(posArray[0]), parseInt(posArray[1]))
         }
+
         if (right == 4) {
             right = 0
         } else {
             right += 1
         }
+
         led.plot(right, left)
     }
 })
